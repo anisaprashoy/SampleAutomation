5aa9d1943184b2b75cad2533e7a001c43a98845b
prashoy <prashoy@192.168.29.87>
2022-09-14 08:26:37 +0530

    First commit.

---
+++ SampleAutomation/src/test/java/test/HelloTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+public class HelloTest 
+{
+	@Test
+	public void testMethod()
+	{
+		System.out.println("I am in  testMethod");
+	}
+}
---
+++ SampleAutomation/src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+public class TestHelloWorld 
+{
+	@Test
+	public void secondTest() 
+	{
+		System.out.println("I am in secondTest");
+	}
+}
